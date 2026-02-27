8a2646926baa92b74f494466e94f97fa5249146e
JAYAKRISHNA T <jayakrishna96555@gmail.com>
2026-02-27 23:43:49 +0530

    Add files via upload

---
+++ Class_and_object.java
@@ -0,0 +1,21 @@
+package basic_programs;
+
+class calss{
+	int sum() {
+		return 1+2;
+	}
+	int sub() {
+		return 1*2;
+	}
+}
+
+public class Class_and_object {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        calss obj = new calss();
+        System.out.println(obj.sum());
+        System.out.println(obj.sub());
+	}
+
+}
