f1c349d4d1cb70b0bd671b329dfd6ad59c231da5
JAYAKRISHNA T <jayakrishna96555@gmail.com>
2026-03-26 23:16:36 +0530

    Add files via upload

---
+++ exception_handling.java
@@ -0,0 +1,28 @@
+package basic_programs;
+
+class jk {
+	int age() throws ClassNotFoundException{
+		return 22;
+	}
+	
+}
+
+public class exception_handling {
+
+	public static void main(String[] args) {
+		
+		int i=0;
+		int j=0;
+		
+		try {
+			 j=i/10;
+		} catch (Exception e) {
+			System.out.println(e);
+			System.out.println(j);
+		}
+		System.out.println("bye");
+		System.out.println(j);
+
+	}
+
+}
