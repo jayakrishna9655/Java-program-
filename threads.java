5bf6de6cf15ff9ac9fdb19e688a4a483f7f02a23
JAYAKRISHNA T <jayakrishna96555@gmail.com>
2026-03-30 22:40:51 +0530

    Add files via upload

--- threads.java
+++ threads.java
@@ -1,26 +1,49 @@
 package basic_programs;
 
-class threadData1 extends Thread{
+//class threadData1 extends Thread{
+//	public void run() {
+//		for(int i=0;i<100;i++) {
+//			System.out.println("hi");
+//		}
+//	}
+//}
+//class threadData2 extends Thread{
+//	public void run() {
+//		for(int i=0;i<100;i++) {
+//			System.out.println("jai");
+//		}
+//	}
+//}
+
+class threadData1 implements Runnable{
 	public void run() {
 		for(int i=0;i<100;i++) {
 			System.out.println("hi");
+			try {
+				Thread.sleep(5);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
 		}
 	}
 }
-class threadData2 extends Thread{
+class threadData2 implements Runnable{
 	public void run() {
 		for(int i=0;i<100;i++) {
 			System.out.println("jai");
 		}
 	}
 }
-public class threads {
 
+public class threads {
 	public static void main(String[] args) {
 		threadData1 obj1=new threadData1();
 		threadData2 obj2=new threadData2();
-		obj1.start();
-		obj2.start();
+		
+		Thread t1=new Thread(obj1);
+		Thread t2=new Thread(obj2);
+		
+		t1.start();
+		t2.start();
 	}
-	
 }
