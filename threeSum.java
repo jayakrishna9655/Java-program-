e486e442ee323baf93fe9d3847cb1fb98fa8c41c
JAYAKRISHNA T <jayakrishna96555@gmail.com>
2026-08-29 23:28:13 +0530

    Add files via upload

--- threeSum.java
+++ threeSum.java
@@ -1,66 +1,67 @@
 package basic_programs;
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class threeSum {
+class threeSumSolution {
+    public List<List<Integer>> threeSum(int[] nums) {
+    	
+    	ArrayList<List<Integer>> list = new ArrayList<List<Integer>>();
+    	
+    	Arrays.sort(nums);
+    	//-4,-1,-1,0,1,2
 
-	public static void main(String[] args) {
-		
-		int[] arr= {-4 , -1 , -1 , 0 , 1 , 2};
-		
-		Arrays.sort(arr);
-		
-		ArrayList<List<Integer>> list = new ArrayList<List<Integer>>();
-		
-		//-4 , -1 , -1 , 0 , 1 , 2
-		
-		
-		for(int i =0;i<arr.length;i++) {
-			int left=i+1;
-			int right=arr.length-1;
-			
-			if(arr[i]>0) {
-				break;
-			}
-			if(i>0 && arr[i] == arr[i-1]){
+    	for(int i=0;i<nums.length;i++) {
+    		int left=i+1;
+    		int right=nums.length-1;
+    		
+    		if(nums[i]>0) {
+    			break;
+    		}
+    		if(i>0 && nums[i] == nums[i-1]) {
     			continue;
     		}
-			
-			while(left<right) {
-					int sum =arr[left]+arr[i]+arr[right];
-					System.out.println("arr[left] "+arr[left]+" arr[i] "+arr[i]+" arr[right] "+arr[right]);
-					if(sum == 0) {
-						list.add(Arrays.asList(arr[left],arr[i],arr[right]));
-						//System.out.println("arr[left] "+arr[left]+" arr[i] "+arr[i]+" arr[right] "+arr[right]);
-						
-						while(left < right && arr[left] == arr[left+1]) {
-		   					left++;
-		   				}
-		   				while(left < right && arr[right] == arr[right-1]) {
-		   					right--;
-		   				}
-						left++;
-						right--;
-					}
-					
-					else if(sum <0){
-						left++;
-					}
-	    			else {
-						right--;
-					}
-				}
-			
-			
-			
-			
-		}
+    		
+    		while(left<right) {
+    			int sum = nums[i] + nums[left] + nums[right];
+    			
+    			System.out.println("nums[i] "+nums[i] +"nums[left] "+ nums[left] + "  nums[right] "+nums[right]);
+    			
+    			if(sum ==0) {
+    				list.add(Arrays.asList(nums[i],nums[left],nums[right]));
+        			
+        			while(left<right && nums[left] == nums[left+1]) {
+        				left++;
+        			}
+        			while(left<right && nums[right] == nums[right-1]) {
+        				right--;
+        			}
+        			left++;
+    				right--;
+    			}
+    			else if(sum<0) {
+    				left++;	
+    			}
+    			else {
+    				right--;
+    			}
+    			
+    		}
+    		
+    		
+    	}
+    	return list;
 		
-		System.out.println(list);
+    }
+}
+
+class threeSum{
+	public static void main(String[] args) {
+		System.out.println("hi");
+		threeSumSolution sol = new threeSumSolution();
+		int[] arr = {0,0,0,0};
+		System.out.println(sol.threeSum(arr));
 		
 	}
-	
 }
